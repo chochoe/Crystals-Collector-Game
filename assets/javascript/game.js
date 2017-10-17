78e377d20949163d032148966012fd0f69b4a4da
Emery Cho <cho.e@live.com>
2017-10-16 23:37:19 -0700

    all i got

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+	var wins = 0;
+
+	var losses = 0;
+
+	// don't know how to put in the function for numbers with predetermined range for clicked crystals and randon number; 
+	// also not able to get the background boxes to show up for the random number and score;
+	// also don't know how to link jewels with click function (also don't know how to code for this) and points;
+	// restart functions also not able to get done;
+	// haven't uploaded to github.io page yet
+
+});
